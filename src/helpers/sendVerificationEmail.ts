18033e0619f4d030196166617519f294d917eeed
Sashreek-PC <sashreek102003@gmail.com>
2024-06-14 13:20:46 +0530

    verificationEmail codeBase

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,1 +1,28 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/verificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string,
+): Promise<ApiResponse> {
+    try {
+
+        await resend.emails.send({
+            from: 'onboarding@resend.dev' ,
+            to: email,
+            subject: 'Mystery Message Verification Code',
+            react: VerificationEmail({ username, otp: verifyCode }),
+        });
+        return { success: true, message: 'Verification email sent successfully.' };
+
+    } catch (emailError) {
+        console.log('Error in sending email: ', emailError);
+        return {
+            success: false,
+            message: 'Failed to send verification code.'
+        }
+    }
+}
